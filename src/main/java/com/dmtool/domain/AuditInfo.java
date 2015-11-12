f59d3237b071f12f84c4189cb018808221179931
punnam <punnamc@gmail.com>
2015-11-12 07:15:14 -0500

    push@7:14AM

---
+++ src/main/java/com/dmtool/domain/AuditInfo.java
@@ -0,0 +1,34 @@
+package com.dmtool.domain;
+
+import java.sql.Timestamp;
+
+public class AuditInfo {
+	private Timestamp createTime;
+	private Timestamp updateTime;
+	private int createUser;
+	private int updateUser;
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public Timestamp getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Timestamp updateTime) {
+		this.updateTime = updateTime;
+	}
+	public int getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(int createUser) {
+		this.createUser = createUser;
+	}
+	public int getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(int updateUser) {
+		this.updateUser = updateUser;
+	}
+}
